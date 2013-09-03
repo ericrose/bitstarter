c9b876c2affec64ae568f5d486f834fc664eabd4
ericrose <eric.rose@gmail.com>
2013-09-02 21:55:47 -0400

    fixed web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function read(err, data){
+    if (err) {
+      throw err;
+    }
+    content = data;
+
+  //response.send('Hello World 2!');
+  response.send(content.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
